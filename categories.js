30aa366c5e77eb8e1ee6e04fa7de4e416f2a9592
JIBIN JV <95421415+jibinjohn22@users.noreply.github.com>
2022-02-11 12:09:55 +0530

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const { poolPromise } = require('../db')
+
+router.get('/', async (req, res) => {
+    try {
+      const pool = await poolPromise
+      const result = await pool.request()
+          .query('select * from categories')      
+  
+      res.json(result.recordset)
+    } catch (err) {
+      res.status(500)
+      res.send(err.message)
+    }
+  })
+
+
+
+module.exports = router
---
+++ order.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const { poolPromise } = require('../db')
+
+router.get('/', async (req, res) => {
+    try {
+      const pool = await poolPromise
+      const result = await pool.request()
+          .query('select * from orders')      
+  
+      res.json(result.recordset)
+    } catch (err) {
+      res.status(500)
+      res.send(err.message)
+    }
+  })
+
+
+
+
+
+
+
+
+module.exports = router
