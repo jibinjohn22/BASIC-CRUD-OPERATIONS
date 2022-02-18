06acfea97ec453050f0aee2ed095ca392034a27d
JIBIN JV <95421415+jibinjohn22@users.noreply.github.com>
2022-02-18 10:47:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,60 +1,61 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const sql = require('mssql')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const sql = require("mssql");
 
-var indexRouter = require('./routes/index');
-var customersRouter = require('./routes/customers');
-var categoriesRouter=require('./routes/categories');
-var brandsRouter = require('./routes/brands');
-var orderItemsRouter= require('./routes/orderItems');
-var ordersRouter=require('./routes/order');
-var productsRouter=require('./routes/products');
-var staffsRouter=require('./routes/staffs');
-var stocksRouter=require('./routes/stocks');
-var storesRouter=require('./routes/stores');
-
+var indexRouter = require("./routes/index");
+var customersRouter = require("./routes/customers");
+var categoriesRouter = require("./routes/categories");
+var brandsRouter = require("./routes/brands");
+var orderItemsRouter = require("./routes/orderItems");
+var ordersRouter = require("./routes/order");
+var productsRouter = require("./routes/products");
+var staffsRouter = require("./routes/staffs");
+var stocksRouter = require("./routes/stocks");
+var storesRouter = require("./routes/stores");
+var productcheckRouter = require("./routes/productcheck");
+var customercheckRouter = require("./routes/customercheck");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/customers', customersRouter);
-app.use('/categories', categoriesRouter);
-app.use('/brands',brandsRouter);
-app.use('/orderItems',orderItemsRouter);
-app.use('/orders',ordersRouter);
-app.use('/products',productsRouter);
-app.use('/staffs',staffsRouter);
-app.use('/stocks',stocksRouter);
-app.use('/stores',storesRouter);
-
+app.use("/", indexRouter);
+app.use("/customers", customersRouter);
+app.use("/categories", categoriesRouter);
+app.use("/brands", brandsRouter);
+app.use("/orderItems", orderItemsRouter);
+app.use("/orders", ordersRouter);
+app.use("/products", productsRouter);
+app.use("/staffs", staffsRouter);
+app.use("/stocks", stocksRouter);
+app.use("/stores", storesRouter);
+app.use("/productcheck", productcheckRouter);
+app.use("/customercheck", customercheckRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // var config = {
@@ -70,7 +71,7 @@ app.use(function(err, req, res, next) {
 // };
 
 // sql.connect(config).then(pool => {
-//   // Query  
+//   // Query
 //   return pool.request().query('select * from login3')
 // }).then(result => {
 //   console.dir(result)
@@ -79,13 +80,11 @@ app.use(function(err, req, res, next) {
 // // ... error checks
 // });
 
-const server =app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   const host = server.address().address;
   const port = server.address().port;
 
   console.log(`Example app listening at http://${host}:${port}`);
-  
-  
 });
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,24 +1,27 @@
-const sql = require('mssql')
+const sql = require("mssql");
 var config = {
-    user: "sa",
-    password: "abc",
-    server: "DESKTOP-8PK2AOR\\SQLEXPRESS",
-    database: "customers",
-    options: {
-      trustedConnection: true
-    },
-    port: '1433',
-    trustServerCertificate: true,
-  };
+  user: "sa",
+  password: "abc",
+  server: "DESKTOP-8PK2AOR\\SQLEXPRESS",
+  database: "customers",
+  options: {
+    trustedConnection: true,
+  },
+  port: "1433",
+  trustServerCertificate: true,
+  multipleStatements: true,
+};
 
 const poolPromise = new sql.ConnectionPool(config)
   .connect()
-  .then(pool => {
-    console.log('Connected to MSSQL')
-    return pool
+  .then((pool) => {
+    console.log("Connected to MSSQL");
+    return pool;
   })
-  .catch(err => console.log('Database Connection Failed! Bad Config: ', err))
+  .catch((err) => console.log("Database Connection Failed! Bad Config: ", err));
+// var connection = mysql.createConnection({multipleStatements: true});
 
 module.exports = {
-  sql, poolPromise
-}
+  sql,
+  poolPromise,
+};
